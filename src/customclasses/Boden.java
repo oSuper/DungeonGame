9135d0596970867d928dd9767722f51ed3f820b9
Super <59453528+oSuper@users.noreply.github.com>
2022-06-01 23:28:46 +0000

    Create Boden.java

---
+++ src/customclasses/Boden.java
@@ -0,0 +1,17 @@
+package customclasses;
+
+public class Boden implements Terrain {
+
+	private String Key = "Boden.png";
+
+	public Boden() {
+		Key = "Boden.png";
+
+	}
+
+	public String gebeSchluessel() {
+		return Key;
+
+	}
+
+}
